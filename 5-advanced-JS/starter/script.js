647d60dff5403bbcbc236e2a4b9d260a113e55f5
Dung Bui <dungdalatds@gmail.com>
2020-07-08 17:25:56 +0700

    section6

---
+++ 5-advanced-JS/starter/script.js
@@ -0,0 +1,44 @@
+// var man=function(name,job,year){
+// 	this.name=name;
+// 	this.job=job;
+// 	this.year=year;
+// 	this.calc=function(){
+// 		console.log(1990-this.year);
+
+// 	}
+// }
+// // man.prototype.calc=function(){
+// // 		console.log(1990-this.year);
+
+// // 	}
+// man.prototype.lastname='nam';
+// dung=new man('Dung','Designer','1980');
+// // console
+// dung.calc();
+// var person={
+// 	calc:function(){
+// 		console.log(1990-this.year);
+// }
+// };
+// var john=Object.create(person);
+// john.year=1980;
+// john.name='john';
+var year=[1990,1992,1994,1993,1999];
+var age=[];
+function arrayCalc(arr,fn){
+	var arrRes=[];
+for(var i=0;i<arr.length-1;i++){
+	arrRes.push(fn(arr[i]));
+}
+
+	return arrRes;
+}
+function calcAge(el){
+	var age=2020-el;
+	return age;
+
+}
+
+age=arrayCalc(year,calcAge) 
+
+
