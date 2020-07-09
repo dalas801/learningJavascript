0e7a3edae7bc7bc356e7334df59ee2e8f05c213e
Dung Bui <dungdalatds@gmail.com>
2020-07-09 17:28:51 +0700

    section5 final

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -82,6 +82,122 @@
 // retirement(2050)(1990);
 
 
-function interViewQuestion(job){
+
+// var john={
+// 	name:'John',
+// 	job:'designer',
+
+// 	presentation:function(style,time){
+// 		if(style==="formal"){
+// 			console.log('Good '+time+', '+this.name+', this is a '+this.job);
+
+// 		}
+// 		else 
+// 		{
+// 		console.log('Blah blah');
+	
+// 		}
+
+// }
+// };
+// var emily={
+// 	name:'Emily',
+// 	job:'Business',
+// };
+// john.presentation('formal','afternoon');
+// john.presentation.call(emily,'formal','morning');
+
+// emilyformal=john.presentation.bind(emily,null,'tonight');
+// emilyformal('formal');
+
+
+// (function(){
+// var question=[];
+
+
+// var questionds=function(question,answer,correctAnswer){
+// 	this.question=question;
+	
+// 	this.correctAnswer=correctAnswer;
+
+// 	this.answer=answer;
+
+// }
+// questionds.prototype.showQuestion=function(){
+// 	console.log(this.question);
+// 	for (var i = 0; i <=this.answer.length - 1; i++) {
+// 		console.log(i+'. '+this.answer[i]);
+// 	}
+// };
+// questionds.prototype.correctQuestion=function(user){
+// 	if(user===this.correctAnswer){
+// 		console.log('That\'s correct');
+// 	}
+// 	else
+// 	{
+// 		console.log('That\'s wrong');
+// 	}
+// };
+// var q1=new questionds('She\'s going to university _____ her studies',['For continuing','For continue'],'0');
+// var q2=new questionds('Sorry, but this chair is ______.',['not usually go','do not go usually'],'1');
+// var q3=new questionds('I ______ bus on Mondays.',['go to work with','go to work by'],'0');
+
+// var question=[q1,q2,q3];
+// var n=Math.floor(Math.random() * question.length); 
+// question[n].showQuestion();
+// var user=prompt('Please choose the answer:');
+// question[n].correctQuestion(user);
+
+// })();
+
+(function(){
+var question=[];
+
+
+var questionds=function(question,answer,correctAnswer){
+	this.question=question;
+	
+	this.correctAnswer=correctAnswer;
+
+	this.answer=answer;
 
 }
+questionds.prototype.showQuestion=function(){
+	console.log(this.question);
+	for (var i = 0; i <=this.answer.length - 1; i++) {
+		console.log(i+'. '+this.answer[i]);
+	}
+};
+questionds.prototype.correctQuestion=function(user){
+	if(user===this.correctAnswer){
+		console.log('That\'s correct');
+	}
+	else
+	{
+		console.log('That\'s wrong');
+	}
+	
+
+};
+var q1=new questionds('She\'s going to university _____ her studies',['For continuing','For continue'],'0');
+var q2=new questionds('Sorry, but this chair is ______.',['not usually go','do not go usually'],'1');
+var q3=new questionds('I ______ bus on Mondays.',['go to work with','go to work by'],'0');
+
+var question=[q1,q2,q3];
+function nextQuestion(){
+var n=Math.floor(Math.random() * question.length); 
+question[n].showQuestion();
+
+if(user!=='exit')
+{
+var user=prompt('Please choose the answer:');
+question[n].correctQuestion(user);
+nextQuestion();
+}
+
+}
+nextQuestion();
+
+})();
+
+
