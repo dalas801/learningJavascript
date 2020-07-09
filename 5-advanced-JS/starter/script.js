69c354f3693b739538c90f1ae415148be29563c6
dungbuids5 <dungdalatds5@gmail.com>
2020-07-09 07:26:36 +0700

    finish section5 tt

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -23,22 +23,65 @@
 // var john=Object.create(person);
 // john.year=1980;
 // john.name='john';
-var year=[1990,1992,1994,1993,1999];
-var age=[];
-function arrayCalc(arr,fn){
-	var arrRes=[];
-for(var i=0;i<arr.length-1;i++){
-	arrRes.push(fn(arr[i]));
+
+// var year=[1990,1992,1994,1993,1999];
+// var age=[];
+// function arrayCalc(arr,fn){
+// 	var arrRes=[];
+// for(var i=0;i<arr.length-1;i++){
+// 	arrRes.push(fn(arr[i]));
+// }
+
+// 	return arrRes;
+// }
+// function calcAge(el){
+// 	var age=2020-el;
+// 	return age;
+
+// }
+
+// age=arrayCalc(year,calcAge) 
+
+
+// function interViewQuestion(job){
+
+// 	if(job==='designer'){
+// 		return function(name)
+// 		{
+// 		console.log("what do you know this interation UI/UX, "+name);
+
+// 	};
+// }
+// 	else{
+// 		return function(name)
+// 		{
+// 		console.log("you dont have a question?, "+name);};
+// 	}
+// }
+// var designerQuestion= interViewQuestion('designer');
+// designerQuestion('Dung');
+// var otherQuestion=interViewQuestion('fd')('Nguyen');
+
+
+// (function(good){
+// 			var number =1;
+// 			console.log(number>good);
+// 			// console.log(good);
+// })(7);
+// (function(el){
+// 	var number =1;
+// 			console.log(number>el);
+// // })(1);
+
+// function retirement(retireYear){
+// 	return function(birthYear){
+// 		console.log(retireYear-(2020-birthYear) + 'la tuoi ve huu');
+// 	}
+
+// }
+// retirement(2050)(1990);
+
+
+function interViewQuestion(job){
+
 }
-
-	return arrRes;
-}
-function calcAge(el){
-	var age=2020-el;
-	return age;
-
-}
-
-age=arrayCalc(year,calcAge) 
-
-
